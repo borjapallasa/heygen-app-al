12688cb1beb896c2a5b86d252abf619782bfd586
borjapallasa <borjapallasa@gmail.com>
2025-10-23 00:21:35 +0200

    Add BannerError component for error messaging

---
+++ src/features/shared/BannerError.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function BannerError({ msg }) {
+  return (
+    <div className="bg-red-50 border border-red-200 text-red-700 rounded-xl p-3 mb-4">
+      {msg}
+    </div>
+  );
+}
