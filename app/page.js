6c2c2734ff1d501b109dd8aae9e6b81fe01e89e4
borjapallasa <borjapallasa@gmail.com>
2025-10-22 21:12:31 +0200

    Dynamically import AvatarApp component

--- app/page.js
+++ app/page.js
@@ -1,4 +1,8 @@
-import AvatarApp from "@/components/AvatarApp";
+import dynamic from "next/dynamic";
+
+// Dynamically import the client component (optional but safe if any `window` access happens)
+const AvatarApp = dynamic(() => import("@/components/AvatarApp"), { ssr: false });
+
 export default function Page() {
   return <AvatarApp />;
 }
