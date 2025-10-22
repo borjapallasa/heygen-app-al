3253abd215d6ae01ac281565bbece9d256d2dd53
borjapallasa <borjapallasa@gmail.com>
2025-10-22 21:12:07 +0200

    Add 'use client' directive to AvatarApp component

--- components/AvatarApp.jsx
+++ components/AvatarApp.jsx
@@ -1,1 +1,6 @@
+"use client";
+
+import React from "react";
+// (…keep all your imports and the rest of the code…)
+// Make sure the last export is a **default** export:
 export default AvatarGroupBubbles;
